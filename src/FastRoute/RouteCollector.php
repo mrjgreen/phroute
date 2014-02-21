6dd03e267e0e7a44d34c96eb9eb22a20e353aca0
joegreen <joegreen@192.168.1.73>
2014-02-21 00:07:58 +0000

    Added Exceptions

--- src/FastRoute/RouteCollector.php
+++ src/FastRoute/RouteCollector.php
@@ -12,36 +12,37 @@ class RouteCollector {
     private function addRoute($httpMethod, $route, $handler) {
         $routeData = $this->routeParser->parse($route);
         $this->dataGenerator->addRoute($httpMethod, $routeData, $handler);
+        return $this;
     }
     
     public function get($route, $handler)
     {
-        $this->addRoute(Route::GET, $route, $handler);
+        return $this->addRoute(Route::GET, $route, $handler);
     }
     
     public function head($route, $handler)
     {
-        $this->addRoute(Route::GET, $route, $handler);
+        return $this->addRoute(Route::GET, $route, $handler);
     }
     
     public function post($route, $handler)
     {
-        $this->addRoute(Route::POST, $route, $handler);
+        return $this->addRoute(Route::POST, $route, $handler);
     }
     
     public function put($route, $handler)
     {
-        $this->addRoute(Route::PUT, $route, $handler);
+        return $this->addRoute(Route::PUT, $route, $handler);
     }
     
     public function delete($route, $handler)
     {
-        $this->addRoute(Route::DELETE, $route, $handler);
+        return $this->addRoute(Route::DELETE, $route, $handler);
     }
     
     public function options($route, $handler)
     {
-        $this->addRoute(Route::OPTIONS, $route, $handler);
+        return $this->addRoute(Route::OPTIONS, $route, $handler);
     }
 
     public function getData() {
