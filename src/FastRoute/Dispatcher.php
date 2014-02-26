280d7c00b16aae54481ca2d5f4ea76d02fbe206a
Joe Green <joe.green0991@gmail.com>
2014-02-26 21:28:52 +0000

    Update Dispatcher.php

--- src/FastRoute/Dispatcher.php
+++ src/FastRoute/Dispatcher.php
@@ -7,10 +7,15 @@ class Dispatcher {
 
     private $staticRouteMap;
     private $variableRouteData;
+    private $after;
+    private $before;
+    private $filters;
 
     public function __construct(RouteCollector $data)
     {
         list($this->staticRouteMap, $this->variableRouteData) = $data->getData();
+        
+        list($this->before, $this->after, $this->filters) = $data->getFilters();
     }
 
     public function dispatch($httpMethod, $uri)
@@ -19,10 +24,55 @@ class Dispatcher {
         {
             $httpMethod = Route::GET;
         }
+                
+        list($beforeFilter, $afterFilter, $handler, $vars) = $this->parseFilters($httpMethod, $uri);
         
+        if(($response = $this->dispatchFilters($beforeFilter)) !== null)
+        {
+            return $response;
+        }
+        
+        $response = call_user_func_array($handler, $vars);
+
+        $this->dispatchFilters($afterFilter);
+        
+        return $response;
+    }
+    
+    private function dispatchFilters($filters, $args = array())
+    {        
+        while($filter = array_shift($filters))
+        {
+            if(($response = call_user_func_array($filter, $args)) !== null)
+            {
+                return $response;
+            }
+        }
+    }
+    
+    private function parseFilters($httpMethod, $uri)
+    {
         list($handler, $vars) = $this->dispatchRoute($httpMethod, $uri);
         
-        return call_user_func_array($handler, $vars);
+        $beforeFilter = array();
+        $afterFilter = array();
+        
+        if(is_array($handler))
+        {
+            if(isset($handler[Route::BEFORE]))
+            {
+                $beforeFilter = array_intersect_key($this->filters, array_flip((array) $handler[Route::BEFORE]));
+            }
+            
+            if(isset($handler[Route::AFTER]))
+            {
+                $afterFilter = array_intersect_key($this->filters, array_flip((array) $handler[Route::AFTER]));
+            }
+            
+            $handler = array_pop($handler);
+        }
+        
+        return array(array_merge($this->before, $beforeFilter), array_merge($this->after, $afterFilter), $handler, $vars);
     }
     
     private function dispatchRoute($httpMethod, $uri)
