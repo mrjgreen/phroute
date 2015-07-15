37082861a27a0c7a4a4b5959271062b7206c80ef
Joe Green <joe.green0991@gmail.com>
2015-07-15 20:40:40 +0100

    Jump through sensio labs hoops for a medal

--- examples/simple.php
+++ examples/simple.php
@@ -12,12 +12,12 @@ $collector->post('products', function(){
     return 'Create Product';
 });
 
-$collector->put('orders/{id}', function($id){
-    return 'Update Order ' . $id;
+$collector->put('items/{id}', function($id){
+    return 'Amend Item ' . $id;
 });
 
 $dispatcher =  new Phroute\Phroute\Dispatcher($collector->getData());
 
 echo $dispatcher->dispatch('GET', '/'), "\n";   // Home Page
 echo $dispatcher->dispatch('POST', '/products'), "\n"; // Create Product
-echo $dispatcher->dispatch('PUT', '/orders/123'), "\n"; // Update Order 123
+echo $dispatcher->dispatch('PUT', '/items/123'), "\n"; // Amend Item 123
